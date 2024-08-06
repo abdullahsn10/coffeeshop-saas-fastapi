882f34d5c0b71c3aedb1f3be7e77925a7947f2b3
Abdullah Naser <Abdullah.Naser@exalt.ps>
2024-08-05 23:59:40 -0700

    setup routers package and register all routes

--- shops_app/main.py
+++ shops_app/main.py
@@ -1,14 +1,17 @@
 from fastapi import FastAPI
+from shops_app.routers import (authentication, branch, coffee_shop,
+                               customer, inventory_item, menu_item,
+                               order, report, user)
 
 app = FastAPI()
 
-
-#register routes
-
-
-@app.get("/")
-def read_root():
-    """
-    root path endpoint
-    """
-    return {"Hello": "World"}
+# register routes
+app.include_router(authentication.router)
+app.include_router(coffee_shop.router)
+app.include_router(user.router)
+app.include_router(branch.router)
+app.include_router(customer.router)
+app.include_router(inventory_item.router)
+app.include_router(menu_item.router)
+app.include_router(order.router)
+app.include_router(report.router)
