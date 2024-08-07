4549470a7db7a584f06b7c9a7a5d8fe512b23022
Abdullah Naser <Abdullah.Naser@exalt.ps>
2024-08-07 02:29:57 -0700

    adding PUT endpoint to update branch

--- src/helpers/branch.py
+++ src/helpers/branch.py
@@ -5,8 +5,10 @@ from src.helpers import coffee_shop
 
 
 def create(
-        request: schemas.BranchBase, coffee_shop_id: int, db: Session,
-        user_coffee_shop_id: int
+        request: schemas.BranchBase,
+        coffee_shop_id: int,
+        db: Session,
+        user_coffee_shop_id: int,
 ) -> models.Branch:
     """
     This helper function will be used to create a new branch
@@ -18,11 +20,13 @@ def create(
     """
     # check if the shop exists
     if not coffee_shop.find_by_id(db=db, id=coffee_shop_id):
-        raise ShopsAppException(f'Coffe Shop with id = {coffee_shop_id} does not exist')
+        raise ShopsAppException(f"Coffe Shop with id = {coffee_shop_id} does not exist")
 
     # check if user authorized to create branch in this shop
     if coffee_shop_id != user_coffee_shop_id:
-        raise ShopsAppUnAuthorizedException('You are not authorized to create branch on this sop')
+        raise ShopsAppUnAuthorizedException(
+            "You are not authorized to create branch on this shop"
+        )
 
     created_branch = models.Branch(
         name=request.name, location=request.location, coffee_shop_id=coffee_shop_id
@@ -44,3 +48,53 @@ def get_coffee_shop_id(branch_id: int, db: Session) -> int:
     """
     branch = db.query(models.Branch).filter(models.Branch.id == branch_id).first()
     return branch.coffee_shop_id
+
+
+def find_by_id(db: Session, id: int) -> models.Branch:
+    """
+    This helper function will be used to find a branch by id
+    :param db: db session
+    :param id: the id of the branch needed to be found
+    :return: the found branch instance
+    """
+    return db.query(models.Branch).filter(models.Branch.id == id).first()
+
+
+def update(request: schemas.BranchBase, db: Session, id: int,
+           coffee_shop_id: int,
+           user_coffee_shop_id: int) -> models.Branch:
+    """
+    This helper function will be used to update a branch
+    :param request: the branch information
+    :param db: db session
+    :param id: the id of the branch needed to be updated
+    :param coffee_shop_id: the coffee shop id of the branch
+    :param user_coffee_shop_id: the user coffee shop id
+    :return: updated branch instance
+    """
+
+    # check coffee_shop exists, branch exits
+    if not coffee_shop.find_by_id(db=db, id=coffee_shop_id):
+        raise ShopsAppException(f'Coffee Shop with id = {coffee_shop_id} does not exist')
+
+    found_branch = find_by_id(db=db, id=id)
+    if not found_branch:
+        raise ShopsAppException(f'Branch with id = {id} does not exist')
+
+    # check if the user authorized to update on this coffee shop
+    if user_coffee_shop_id != coffee_shop_id:
+        raise ShopsAppUnAuthorizedException(f'You are not authorized to update branch on this shop')
+
+    # check if the branch belongs to this coffee shop
+    if get_coffee_shop_id(db=db, branch_id=found_branch.id) != coffee_shop_id:
+        raise ShopsAppException(f'Branch with id = {id} does not exist in this coffee shop')
+
+    # Update all fields of the branch object based on the request
+    update_data = request.model_dump(
+        exclude_unset=True
+    )  # Get dictionary of all set fields in request
+    for field, value in update_data.items():
+        setattr(found_branch, field, value)
+    db.commit()
+    db.refresh(found_branch)
+    return found_branch
--- src/routers/coffee_shop.py
+++ src/routers/coffee_shop.py
@@ -14,19 +14,21 @@ router = APIRouter(
 
 @router.put("/{coffee_shop_id}")
 def update_coffee_shop_endpoint(
-        coffee_shop_id: int,
-        request: schemas.CoffeeShopBase,
-        db: Session = Depends(get_db),
-        current_user: schemas.TokenData = Depends(require_role([models.UserRole.ADMIN])),
+    coffee_shop_id: int,
+    request: schemas.CoffeeShopBase,
+    db: Session = Depends(get_db),
+    current_user: schemas.TokenData = Depends(require_role([models.UserRole.ADMIN])),
 ):
     """
-        PUT endpoint to fully update a specific coffee shop
+    PUT endpoint to fully update a specific coffee shop
     """
     try:
-        return coffee_shop.update(request=request,
-                                  db=db,
-                                  id=coffee_shop_id,
-                                  user_coffe_shop_id=current_user.coffee_shop_id)
+        return coffee_shop.update(
+            request=request,
+            db=db,
+            id=coffee_shop_id,
+            user_coffe_shop_id=current_user.coffee_shop_id,
+        )
     except ShopsAppException as se:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=str(se))
     except ShopsAppUnAuthorizedException as ua:
@@ -37,18 +39,46 @@ def update_coffee_shop_endpoint(
         )
 
 
-@router.post('/{coffee_shop_id}/branches')
-def create_branch_endpoint(coffee_shop_id: int, request: schemas.BranchBase,
-                           db: Session = Depends(get_db),
-                           current_user: schemas.TokenData = Depends(require_role([models.UserRole.ADMIN])),
-                           ):
+@router.post("/{coffee_shop_id}/branches")
+def create_branch_endpoint(
+    coffee_shop_id: int,
+    request: schemas.BranchBase,
+    db: Session = Depends(get_db),
+    current_user: schemas.TokenData = Depends(require_role([models.UserRole.ADMIN])),
+):
     """
     POST endpoint to create a branch for a specific coffee shop
     """
     try:
         return branch.create(
-            request=request, coffee_shop_id=coffee_shop_id, db=db, user_coffee_shop_id=current_user.coffee_shop_id
+            request=request,
+            coffee_shop_id=coffee_shop_id,
+            db=db,
+            user_coffee_shop_id=current_user.coffee_shop_id,
         )
+    except ShopsAppException as se:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=str(se))
+    except ShopsAppUnAuthorizedException as ua:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=str(ua))
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e)
+        )
+
+
+@router.put('/{coffee_shop_id}/branches/{branch_id}')
+def update_branch_endpoint(coffee_shop_id: int, branch_id: int, request: schemas.BranchBase,
+                           db: Session = Depends(get_db),
+                           current_user: schemas.TokenData = Depends(require_role([models.UserRole.ADMIN]))
+                           ):
+    """
+    PUT endpoint to update a specific branch for a specific coffee shop
+    """
+
+    try:
+        return branch.update(request=request, db=db, id=branch_id,
+                      coffee_shop_id= coffee_shop_id,
+                      user_coffee_shop_id=current_user.coffee_shop_id)
     except ShopsAppException as se:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=str(se))
     except ShopsAppUnAuthorizedException as ua:
