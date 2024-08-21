bda0ec9044c19e821809987696fe0152200e06c9
Abdullah S. Naser <121259956+abdullahsn10@users.noreply.github.com>
2024-08-21 08:55:58 +0300

    Order Report: GET total orders, total income (#14)
    * Add GET endpoint for /reports/ to get total income and orders
    
    * Small update in orders report
    
    ---------
    
    Co-authored-by: Abdullah Naser <Abdullah.Naser@exalt.ps>

--- src/routers/report.py
+++ src/routers/report.py
@@ -80,43 +80,6 @@ def list_chefs_orders_endpoint(
 
 
 @router.get(
-    "/coffee-shops/{coffee_shop_id}/chefs-orders",
-    response_model=list[schemas.ChefOrderReport],
-)
-def list_chefs_orders_endpoint(
-    coffee_shop_id: int,
-    from_date: date,
-    to_date: date,
-    order_by: str = Query(None, regex="^(served_orders)$"),
-    sort: str = Query(None, regex="^(asc|desc)$"),
-    db: Session = Depends(get_db),
-    current_user: schemas.TokenData = Depends(require_role([UserRole.ADMIN])),
-):
-    """
-    GET endpoint to list all chefs with their served orders
-    """
-    try:
-        check_if_user_can_access_shop(
-            user_coffee_shop_id=current_user.coffee_shop_id,
-            target_coffee_shop_id=coffee_shop_id,
-        )
-        return report.list_chefs_orders(
-            db=db,
-            coffee_shop_id=coffee_shop_id,
-            from_date=from_date,
-            to_date=to_date,
-            order_by=order_by,
-            sort=sort,
-        )
-    except ShopsAppException as se:
-        raise HTTPException(status_code=se.status_code, detail=se.message)
-    except Exception as e:
-        raise HTTPException(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e)
-        )
-
-
-@router.get(
     "/coffee-shops/{coffee_shop_id}/issuers-orders",
     response_model=list[schemas.IssuerOrderReport],
 )
@@ -144,6 +107,36 @@ def list_issuers_orders_endpoint(
             to_date=to_date,
             order_by=order_by,
             sort=sort,
+        )
+    except ShopsAppException as se:
+        raise HTTPException(status_code=se.status_code, detail=se.message)
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e)
+        )
+
+
+@router.get("/coffee-shops/{coffee_shop_id}/orders-income")
+def list_orders_income_endpoint(
+    coffee_shop_id: int,
+    from_date: date,
+    to_date: date,
+    db: Session = Depends(get_db),
+    current_user: schemas.TokenData = Depends(require_role([UserRole.ADMIN])),
+):
+    """
+    GET endpoint to list total income from orders along with the number of orders
+    """
+    try:
+        check_if_user_can_access_shop(
+            user_coffee_shop_id=current_user.coffee_shop_id,
+            target_coffee_shop_id=coffee_shop_id,
+        )
+        return report.list_orders_income(
+            db=db,
+            coffee_shop_id=coffee_shop_id,
+            from_date=from_date,
+            to_date=to_date,
         )
     except ShopsAppException as se:
         raise HTTPException(status_code=se.status_code, detail=se.message)
